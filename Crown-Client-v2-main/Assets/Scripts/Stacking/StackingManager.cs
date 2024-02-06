e983c2af4b7cdf676787fb848003828868299411
youngD23 <88158631+youngD23@users.noreply.github.com>
2024-02-05 20:20:58 -0500

    Fixed Stacking Game
    Fixed basket movement to reach entire screen, increased speed of falling objects, and made code more modular

--- Crown-Client-v2-main/Assets/Scripts/Stacking/StackingManager.cs
+++ Crown-Client-v2-main/Assets/Scripts/Stacking/StackingManager.cs
@@ -9,28 +9,36 @@ public class StackingManager : MonoBehaviour
     public GameObject[] fallingObject;
 
     private float randX = 0;
-
     private float timer = 0;
+    private float timeUntilNextObject = 1;
+    private int winningScore = 25;
 
     public Text score;
-
     public TMP_Text win;
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        
+    
+    void Update() {
+        InstantiateObjects();
+        UpdateScore();
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        if (timer >= 2)
-        {
+    private void UpdateScore() {
+        score.text = "Score: " + StackerMovement.instance.stacked.ToString();
+        if (StackerMovement.instance.stacked == winningScore) {
+            Time.timeScale = 0;
+            win.gameObject.SetActive(true);
+        }
+    }
+
+    /// <summary>
+    /// 
+    /// </summary>
+    void InstantiateObjects() {
+        if (timer >= timeUntilNextObject) {
             float randObj = Random.Range(0, 2);
 
-            randX = Random.Range(-2.0f, 2.0f);
-            randObj = Random.Range(0.0f, 2.0f);
+            randX = Random.Range(-10f, 10f); //Range Objects will fall within
+            randObj = Random.Range(0f, 2f);
             Debug.Log(randObj);
             if (randObj <= 1 && randObj >= 0) randObj = 0;
             if (randObj <= 2 && randObj >= 1.1f) randObj = 1;
@@ -40,16 +48,10 @@ public class StackingManager : MonoBehaviour
             {
                 fallingObject[i].GetComponent<Physics>().;
             }*/
+            timeUntilNextObject = Random.Range(0.3f, 3f);
             timer = 0;
-        }
-        else
+        } else {
             timer += Time.deltaTime;
-
-        score.text = "Score: " + StackerMovement.instance.stacked.ToString();
-        if (StackerMovement.instance.stacked == 15)
-        {
-            Time.timeScale = 0;
-            win.gameObject.SetActive(true);
         }
     }
 
