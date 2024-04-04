00f3536afcac597728a0cf9f3cf85a2d5ffc5757
youngD23 <88158631+youngD23@users.noreply.github.com>
2024-04-04 01:18:55 -0400

    Created FishManager script and added QuickClick button

--- Crown-Client-v2-main/Assets/Scripts/FishingGame/FishButton.cs
+++ Crown-Client-v2-main/Assets/Scripts/FishingGame/FishButton.cs
@@ -4,5 +4,23 @@ using UnityEngine;
 
 public class FishButton : MonoBehaviour
 {
-    
+    protected GameObject fishManager;
+    protected bool successfullyClicked = false;
+    float destroyTimer = 2f;
+
+    private void Awake() {
+        SetPosition();
+        fishManager = GameObject.FindGameObjectWithTag("FishManager");
+    }
+    void SetPosition() {
+        int xPosition = Random.Range(-300, 300);
+        int yPosition = Random.Range(-150, 150);
+        transform.position = new Vector2(xPosition, yPosition);
+    }
+    protected void TimedDestroy() {
+        destroyTimer -= Time.deltaTime;
+        if (destroyTimer <= 0) {
+            Destroy(this.gameObject);
+        }
+    }
 }
--- Crown-Client-v2-main/Assets/Scripts/FishingGame/QuickButton.cs
+++ Crown-Client-v2-main/Assets/Scripts/FishingGame/QuickButton.cs
@@ -1,8 +1,33 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
-public class QuickButton : MonoBehaviour
+public class QuickButton : FishButton
 {
-    
+    float xScale = 1;
+    float yScale = 1;
+
+    private void Update() {
+        if (successfullyClicked) {
+            TimedDestroy();
+        } else {
+            Shrink();
+        }
+    }
+
+    void Shrink() {
+        xScale -= Time.deltaTime/2;
+        yScale -= Time.deltaTime/2;
+        transform.localScale = new Vector2(xScale, yScale);
+        if (xScale <= 0) {
+            fishManager.GetComponent<FishManager>().ButtonMissed();
+            Destroy(this.gameObject);
+        }
+    }
+    public void Clicked() {
+        GetComponent<Image>().color = Color.green;
+        successfullyClicked = true;
+        fishManager.GetComponent<FishManager>().ButtonClicked();
+    }
 }
